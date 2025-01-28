ad5fdadd726edc02ec4e22e784c6fe9aceacc55e
Philip Brohan <philip@brohan.org>
2025-01-28 21:30:15 +0000

    Environment and data collection

---
+++ images/download_sample.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Download a single Archive file (Devon stations 1941-50)
+
+wget https://digital.nmla.metoffice.gov.uk/download/file/IO_b8032732-5490-406d-8717-c8e88037faec --output-document=pdfs/Devon_1941-1950_RainNos_1651-1689.pdf
