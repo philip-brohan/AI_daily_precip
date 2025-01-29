3e01b101044dc1f16edd589cb9c26c1273dc037a
Philip Brohan <philip@brohan.org>
2025-01-29 11:05:16 +0000

    Initialise  pages branch

--- images/pdf_to_images_sample.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Stop imagemagick from using all the memory and crashing vdi.
-export MAGICK_MAP_LIMIT=2GB
-export MAGICK_MEMORY_LIMIT=2GB
-
-magick -density 300 ./pdfs/Devon_1941-1950_RainNos_1651-1689.pdf jpgs_300dpi/Devon_1941-1950_RainNos_1651-1689.jpg
